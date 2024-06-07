843ba0f1548fed0dbf4306e416069fbeb6623ea3
Shilpa Gopi <Sgopi3284@conestogac.on.ca>
2024-06-07 13:28:34 -0400

    Code review and updating

--- Program.cs
+++ Program.cs
@@ -9,13 +9,16 @@ namespace VirtualPet
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            Console.WriteLine("------------------------------------------\n\n");
             Console.WriteLine("Welcome to Virtual  Pet Game....!!!!\n\n"); //Welcome Message 
 
 
+            Console.WriteLine("------------------------------------------");
             Console.WriteLine("Please Choose a type of PET\n\n"); // Option to choose pet type
 
-
+            
             Console.WriteLine("1. Cat");
             Console.WriteLine("2. Dog");
             Console.WriteLine("3. Rabbit");
@@ -27,25 +30,29 @@ namespace VirtualPet
             switch (input)
             {
                 case "1":
+                    Console.WriteLine("------------------------------------------");
                     Console.WriteLine("You selected Cat...!, What would you like to name your Cat?"); // Accepts name for pet
                     petName = Console.ReadLine();
                     break;
                 case "2":
+                    Console.WriteLine("------------------------------------------");
                     Console.WriteLine("You selected Dog...! ,  What would you like to name your Dog?");// Accepts name for pet
                     petName = Console.ReadLine();
                     break;
                 case "3":
+                    Console.WriteLine("------------------------------------------");
                     Console.WriteLine("You selected Rabbit...!, What would you like to name your Rabbit?");// Accepts name for pet
                     petName = Console.ReadLine();
                     break;
                 default:
+                    Console.WriteLine("------------------------------------------");
                     Console.WriteLine("Invalid entry. Please enter 1, 2, or 3.");// Validation for invalid entry
                     break;
 
             }
-
+            Console.WriteLine("------------------------------------------");
             Console.WriteLine("\nYour pet's name is :" + petName); // to display pet name
-            Console.WriteLine("\nWelcome " + petName + "!" + "" + " Let's take good care of him");
+            Console.WriteLine("\nWelcome " + petName  + ":)" + " Let's take good care of him!");
 
             // declaring hunger,happiness and health values
 
@@ -72,9 +79,7 @@ namespace VirtualPet
                     Console.WriteLine("You feed " + petName + ". His hunger decreases and health improve slighlty!!");
 
 
-                    // Displays feeding related text if user selected option 1
-
-
+                    // Displays feeding related text if user selected option
                 }
 
                 if (menuInput == "2")
@@ -94,7 +99,7 @@ namespace VirtualPet
                 {
                     health = health + 2;
                     happiness--;
-                    Console.WriteLine("Your " + petName + " took rest, His health increases but his happiness decreased!");
+                    Console.WriteLine("Your " + petName + " took rest, His health increases but his happiness decreased & he needs to eat!");
 
 
                     // Displays restinging related text if user selected option 3
@@ -110,8 +115,12 @@ namespace VirtualPet
 
                     if (hunger > 6) // if hunger is greater than 6 then message to feed the pet
                     {
-                        Console.WriteLine("Alert!!!: " + petName + " is hungry! Please feed your ." + petName);
+                        Console.WriteLine("Alert!!!: " + petName + " is hungry, pet is refusing to play! Please feed " + petName);
                     }
+                    //if (hunger < 0) // Ensuring hunger doesn't go below 0
+                        //hunger = 0;
+                    if (hunger == 1) // Alerting to stop feeding if hunger is 1
+                        Console.WriteLine("Alert!!!: " + petName + " is full.Stop Feeding to proceed...!!");
 
                     if (health < 6)
                     {
