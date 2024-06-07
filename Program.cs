4670a880c3b54d9c3d49f13bc57c38bc1f68f8d2
Shilpa Gopi <Sgopi3284@conestogac.on.ca>
2024-06-07 15:37:08 -0400

    Code review. Adding comments and beautifying code.

--- Program.cs
+++ Program.cs
@@ -13,13 +13,14 @@ namespace VirtualPet
 
         {
             Console.WriteLine("------------------------------------------\n\n");
+
             Console.WriteLine("Welcome to Virtual  Pet Game....!!!!\n\n"); //Welcome Message 
 
 
             Console.WriteLine("------------------------------------------");
             Console.WriteLine("Please Choose a type of PET\n\n"); // Option to choose pet type
 
-            
+            // Displays each pet line by line 
             Console.WriteLine("1. Cat");
             Console.WriteLine("2. Dog");
             Console.WriteLine("3. Rabbit");
@@ -28,7 +29,7 @@ namespace VirtualPet
             string petName = "";
 
 
-            switch (input)
+            switch (input) // On giving pets name, name appear here with Pet type. 
             {
                 case "1":
                     Console.WriteLine("------------------------------------------");
@@ -53,7 +54,7 @@ namespace VirtualPet
             }
             Console.WriteLine("------------------------------------------");
             Console.WriteLine("\nYour pet's name is :" + petName); // to display pet name
-            Console.WriteLine("\nWelcome " + petName  + ":)" + " Let's take good care of him!");
+            Console.WriteLine("\nWelcome " + petName + ":)" + " Let's take good care of him!");
 
             // declaring hunger,happiness and health values
 
@@ -64,51 +65,51 @@ namespace VirtualPet
             while (true)
 
             {
-                // THis is main menu and options are listed
+                // This is main menu and options are listed
                 Console.WriteLine("\nMenu:");
-                Console.WriteLine("1.Feed " + petName);
-                Console.WriteLine("2.Play with  " + petName);
-                Console.WriteLine("3.Let " + petName + " Take Rest");
-                Console.WriteLine("4.Check " + petName + "'s status");
-                Console.WriteLine("5.Exit");
+                Console.WriteLine("1.Feed " + petName); // To feed pet
+                Console.WriteLine("2.Play with  " + petName); // To play with pet
+                Console.WriteLine("3.Let " + petName + " Take Rest"); //To rest
+                Console.WriteLine("4.Check " + petName + "'s status"); // Checking status of the pet 
+                Console.WriteLine("5.Exit"); // exits user from the application
                 string menuInput = Console.ReadLine(); // Accepts user's input
+
+                // If user chooses option 1
 
                 if (menuInput == "1")
                 {
                     hunger -= 2;// decreases hunger on playing
-                    health++;
-                    Console.WriteLine("You feed " + petName + ". His hunger decreases and health improve slighlty!!");
+                    health++; // health increases slightly
+                    Console.WriteLine("You feed " + petName + ". His hunger decreases and health improve slighlty!!"); // response message
 
 
                     // Displays feeding related text if user selected option
                 }
+                // If user chooses option 2
 
                 if (menuInput == "2")
                 {
-                    happiness = happiness + 2;
-                    hunger++;
+                    happiness = happiness + 2; // happiness increases
+                    hunger++; // Hunger slightly increases
                     Console.WriteLine("You played with " + petName + ". His happiness increases but he is bit hungry!");
 
                     // Displays playing related text if user selected option 2
 
-
-
-
                 }
-
+                // If user chooses option 3
                 if (menuInput == "3")
                 {
-                    health = health + 2;
-                    happiness--;
-                    hunger++;
+                    health = health + 2; // Health increases
+                    happiness--; // Happiness slightly decreases
+                    hunger++;  // Hunger slightly increases
                     Console.WriteLine("Your " + petName + " took rest, His health increases but his happiness decreased & he needs to eat!");
 
 
-                    // Displays restinging related text if user selected option 3
+                    // Displays resting related text if user selected option 3
 
 
                 }
-
+                // If user chooses option 4
 
                 if (menuInput == "4")
                 {
@@ -119,8 +120,8 @@ namespace VirtualPet
                     {
                         Console.WriteLine("Alert!!!: " + petName + " is hungry, pet is refusing to play! Please feed " + petName);
                     }
-                    //if (hunger < 0) // Ensuring hunger doesn't go below 0
-                        //hunger = 0;
+                    
+                    
                     if (hunger == 1) // Alerting to stop feeding if hunger is 1
                         Console.WriteLine("Alert!!!: " + petName + " is full.Stop Feeding to proceed...!!");
 
@@ -129,14 +130,14 @@ namespace VirtualPet
                         Console.WriteLine("Alert!" + petName + "'s health is decreasing, please give him some rest");
 
                         //If health decreases , giving alert to user asking for REST
- 
+
 
                     }
 
 
                     // Displays restinging related text if user selected option 3
                 }
-
+                // If user chooses option 5
                 if (menuInput == "5")
                 {
                     Console.WriteLine("Thank you for playing with " + petName + "!!"); //Option to exit from the game if the user selected option 5.
@@ -144,7 +145,7 @@ namespace VirtualPet
 
                     break;
 
-                    
+
 
                 }
 
